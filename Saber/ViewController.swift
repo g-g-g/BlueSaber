c2f1c1e7620250f6dc37cc1bf3ebc42f0361840d
Gautham <gautham.gg@outlook.com>
2016-06-10 11:06:52 +0900

    new

--- Saber/ViewController.swift
+++ Saber/ViewController.swift
@@ -8,20 +8,16 @@
 
 import UIKit
 import AVFoundation
-import iAd
 import CoreMotion
 import AudioToolbox
 import GoogleMobileAds
 import Foundation
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController, ADBannerViewDelegate {
+class ViewController: UIViewController {
     
     @IBOutlet weak var lightSaber: UIImageView!
-    @IBOutlet weak var Banner: ADBannerView!
-    @IBOutlet weak var Share: UILabel!
-    @IBOutlet weak var bannerView: GADBannerView!
-    
-    var interstitial: GADInterstitial!
     
     var ButtonAudioURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("On", ofType: "wav")!)
     var ButtonAudio2URL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Off", ofType: "wav")!)
@@ -40,19 +36,6 @@ class ViewController: UIViewController, ADBannerViewDelegate {
         do { self.ButtonAudioPlayer3 = try AVAudioPlayer(contentsOfURL:self.ButtonAudio3URL, fileTypeHint: nil) } catch _ { }
 
         self.lightSaber.hidden = true
-
-        self.Banner.hidden = true
-        self.bannerView.hidden = false
-        Banner.delegate = self
-        self.canDisplayBannerAds = true
-        print("Google Mobile Ads SDK version: " + GADRequest.sdkVersion())
-        bannerView.adUnitID = "ca-app-pub-6037554166107597/7563075862"
-        bannerView.rootViewController = self
-        bannerView.loadRequest(GADRequest())
-        self.interstitial = GADInterstitial(adUnitID: "ca-app-pub-6037554166107597/4470008669")
-        let request = GADRequest()
-        request.testDevices = ["2077ef9a63d2b398840261c8221a0c9b"]
-        self.interstitial.loadRequest(request)
         
     }
     
@@ -84,24 +67,6 @@ class ViewController: UIViewController, ADBannerViewDelegate {
         dispatch_after(
             dispatch_time( DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC))), dispatch_get_main_queue(), closure)
     }
-   
-    func bannerView(banner: ADBannerView!, didFailToReceiveAdWithError error: NSError!) {
-        NSLog("Error")
-        self.bannerView.hidden = false
-    }
-    
-    func bannerViewWillLoadAd(banner: ADBannerView!) {
-    
-    }
-    
-    func bannerViewActionShouldBegin(banner: ADBannerView!, willLeaveApplication willLeave: Bool) -> Bool {
-        return true
-    }
-    
-    func bannerViewDidLoadAd(banner: ADBannerView!) {
-        self.bannerView.hidden = true
-        self.Banner.hidden = false
-    }
 
     @IBAction func buttonTouchDown(sender: UIButton) {
         ButtonAudioPlayer.play()
@@ -110,19 +75,14 @@ class ViewController: UIViewController, ADBannerViewDelegate {
         self.lightSaber.hidden = false
         lightSaber.center.y = self.view.frame.width + 1000  //Starting Location
         UIView.animateWithDuration(1.0  /*Duration*/, animations: ({
-            self.lightSaber.center.y = self.view.frame.width - 30  /*Ending location*/}))
+            self.lightSaber.center.y = self.view.frame.width - 50  /*Ending location*/}))
         
         AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
-        
-        UIView.animateWithDuration(0.4, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
-            self.Share.alpha = 0.0
-            }, completion: {
-                (finished: Bool) -> Void in
-        })
         
         delay(0.5){
         self.toggleTorch(on: true)
         }
+    
     }
     
     @IBAction func buttonTouchEnded(sender: UIButton) {
@@ -131,14 +91,10 @@ class ViewController: UIViewController, ADBannerViewDelegate {
         ButtonAudioPlayer2.play()
         ButtonAudioPlayer3.stop()
 
-        lightSaber.center.y = self.view.frame.width - 30   //Starting location
+        lightSaber.center.y = self.view.frame.width - 50   //Starting location
         UIView.animateWithDuration(1.0 /*Duration*/, animations: ({self.lightSaber.center.y = self.view.frame.width + 1000    /*Ending location*/}))
         
         AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
-        
-        UIView.animateWithDuration(0.8, delay: 0.0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
-            self.Share.alpha = 1.0
-            }, completion: nil)
         
         delay(0.5){
         self.toggleTorch(on: false)
@@ -150,14 +106,5 @@ class ViewController: UIViewController, ADBannerViewDelegate {
         }
         
     }
-
-    @IBAction func shareButton(sender: UIButton) {
-
-        let myShare = "Simulate a lightsaber on the iPhone app, Blue Saber! Download at https://appsto.re/us/Wu6iab.i" //Text to share
-        let activityVC:UIActivityViewController = UIActivityViewController(activityItems: [myShare], applicationActivities: nil)
-        self.presentViewController(activityVC, animated: true, completion: nil)
-        lightSaber.center.y = self.view.frame.width - 80
-        UIView.animateWithDuration(1.0 , animations: ({self.lightSaber.center.y = self.view.frame.width + 1000}))
-        self.lightSaber.hidden = true
-    }
+    
 }
